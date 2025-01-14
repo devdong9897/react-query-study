a4e0fef69ca6ca0d8d41ccc7b84cdec81920be3d
devdong9897 <dev.dong9897@gmail.com>
2025-01-14 17:02:48 +0900

    feat: useQuery훅을 사용해 데이터 가져오는 법

--- src/pages/ReactQueryPage.jsx
+++ src/pages/ReactQueryPage.jsx
@@ -1,7 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 const ReactQueryPage = () => {
-  return <div>ReactQueryPage</div>;
+  const fetchPost = () => {
+    return axios.get("http://localhost:3004/posts");
+  };
+  // useQuery: 데이터를 서버에서 가져오는 기능을 하는 React Hook.
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPost,
+  });
+  console.log("ddd", data, isLoading);
+  console.log("error", isError, error);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  return (
+    <div>
+      {data.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ReactQueryPage;
