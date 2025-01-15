cfbe0086d7e4cba2e122aa2f12dbcb5c9588c7b0
devdong9897 <dev.dong9897@gmail.com>
2025-01-15 15:10:46 +0900

    feat: 여러개의 요청 결과를 하나로 합치기

--- src/pages/NormalPage.jsx
+++ src/pages/NormalPage.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
+import { usePostQuery } from "../hooks/usePosts";
 
 export const NormalPage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [data, setData] = useState([]);
 
-  const fetchPost = async () => {
-    setIsLoading(true);
-    const url = "http://localhost:3004/posts";
-    const response = await fetch(url);
-    const data = await response.json();
-    setIsLoading(false);
-    setData(data);
-  };
+  // const fetchPost = async () => {
+  //   setIsLoading(true);
+  //   const url = "http://localhost:3004/posts";
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   setIsLoading(false);
+  //   setData(data);
+  // }'
 
-  useEffect(() => {
-    fetchPost();
-  }, []);
+  // useEffect(() => {
+  //   fetchPost();
+  // }, []);
+
+  const { data, isLoading, isError, error } = usePostQuery();
 
   if (isLoading) {
     return <h1>Loading...</h1>;
--- src/pages/ReactQueryPage.jsx
+++ src/pages/ReactQueryPage.jsx
@@ -1,38 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import React from "react";
+import { usePostQuery } from "../hooks/usePosts";
+import { useQueries } from "@tanstack/react-query";
+import axios from "axios";
+import { data } from "react-router-dom";
 
 const ReactQueryPage = () => {
-  const fetchPost = (queryData) => {
-    const id = queryData.queryKey[1];
+  const ids = [1, 2, 3, 4];
+
+  const fetchPostDetail = (id) => {
     return axios.get(`http://localhost:3004/posts/${id}`);
   };
-  // useQuery: 데이터를 서버에서 가져오는 기능을 하는 React Hook.
-  const { isLoading, data, isError, error, refetch } = useQuery({
-    queryKey: ["posts", 1],
-    queryFn: fetchPost,
-    retry: 1,
-    select: (data) => {
-      return data.data;
+
+  const results = useQueries({
+    queries: ids.map((id) => {
+      return {
+        queryKey: ["posts", id],
+        queryFn: () => fetchPostDetail(id),
+      };
+    }),
+    // combine: 여러 개의 요청 결과를 하나로 합치는 역할.
+    combine: (results) => {
+      return {
+        data: results.map((result) => result.data?.data),
+      };
     },
   });
-  console.log("ddd", data, isLoading);
-  console.log("error", isError, error);
 
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-  if (isError) {
-    return <h1>{error.message}</h1>;
-  }
-  return (
-    <div>
-      {/* {data?.map((item) => (
-        <div key={item.id}>{item.title}</div>
-      ))} */}
-      <button onClick={refetch}>post리스트 다시 들고오기</button>
-    </div>
-  );
+  console.log("rrr", results);
+  return <div></div>;
 };
 
 export default ReactQueryPage;
